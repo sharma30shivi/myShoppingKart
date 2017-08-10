7ee108d7d0362153cb7655c1c9bd46352accb0cb
sharma30shivi <sharma30shivi@gmail.com>
2017-08-11 00:51:39 +0530

    Completed the interface theme and other issues

--- myshoppingkart/src/main/java/com/shivi/myshoppingkart/controller/PageController.java
+++ myshoppingkart/src/main/java/com/shivi/myshoppingkart/controller/PageController.java
@@ -13,10 +13,29 @@ public class PageController {
 	@RequestMapping(value={"/", "/home", "/index"})
 	public ModelAndView index(){
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to My First Spring Mvc");
+		//mv.addObject("greeting", "Welcome to My First Spring Mvc");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome",true);
 		return mv;
 	}
 	
+	@RequestMapping(value={"about"})
+	public ModelAndView about(){
+		ModelAndView mv = new ModelAndView("page");
+		//mv.addObject("greeting", "Welcome to My First Spring Mvc");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout",true);
+		return mv;
+	}
+	
+	@RequestMapping(value={"contact"})
+	public ModelAndView contact(){
+		ModelAndView mv = new ModelAndView("page");
+		//mv.addObject("greeting", "Welcome to My First Spring Mvc");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact",true);
+		return mv;
+	}
 	/*@RequestMapping(value="/test")
 	public ModelAndView test(@RequestParam(value= "greeting", required=false) String greeting){
 		if(greeting == null){
@@ -26,7 +45,7 @@ public class PageController {
 		mv.addObject("greeting", greeting);
 		return mv;
 	}*/
-	
+	/*
 	@RequestMapping(value="/test/{greeting}")
 	public ModelAndView test(@PathVariable("greeting") String greeting){
 		if(greeting == null){
@@ -35,5 +54,5 @@ public class PageController {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", greeting);
 		return mv;
-	}
+	}*/
 }
